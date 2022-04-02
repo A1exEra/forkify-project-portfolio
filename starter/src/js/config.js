df3135c3117a244ed6fdb43a6c815098a35cdff1
A1exEra <eramisant@yahoo.com>
2022-04-02 20:08:48 +0100

    Initial commit

---
+++ starter/src/js/config.js
@@ -0,0 +1,9 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+
+export const TIMEOUT_SEC = 10;
+//number of dislayed utems in search page
+export const RES_PER_PAGE = 10;
+//must go to forkify wesb site and get a unique generated API KEY FIRST!!!
+export const KEY = '0d80a6ae-ecd1-40c8-9381-1b794b9c9074';
+export const MODAL_CLOSE_SEC = 2.5;
+////////////////////////////////////
